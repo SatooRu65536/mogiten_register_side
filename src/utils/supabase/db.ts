45e0df895049c640224e578ecdb75cbe28729353
satooru65536 <ryuto4619@gmail.com>
2024-10-12 10:51:04 +0900

    feat: done

--- src/utils/supabase/db.ts
+++ src/utils/supabase/db.ts
@@ -1,20 +1,19 @@
+import { itemList } from '../../const/items';
 import { OrderItem } from '../../types';
 import { supabase } from './init';
-import { ofetch } from 'ofetch';
-import { joinURL } from 'ufo';
+// import { ofetch } from 'ofetch';
+// import { joinURL } from 'ufo';
 
 const KEY_URL = import.meta.env.VITE_KEY_URL;
 
 export async function addOrder(order: OrderItem[], key: string) {
-  console.log('key:', key);
-  const res = await ofetch(joinURL(KEY_URL, key), {
-    parseResponse: JSON.parse,
-  });
-
-  const { id } = res;
+  // console.log('key:', key);
+  // const res = await ofetch(joinURL(KEY_URL, key), {
+  //   parseResponse: JSON.parse,
+  // });
 
   await supabase.from('order').insert({
-    order: order.map((item) => ({ ...item })),
-    user_id: id,
+    order: { order: order.map((item) => ({ ...item })), itmes: itemList },
+    user_id: 0,
   });
 }
---
+++ supabase/migrations/20241012014305_add-status-column.sql
@@ -0,0 +1,1 @@
+alter table "public"."order" add column "status" text default 'cooking'::text;
