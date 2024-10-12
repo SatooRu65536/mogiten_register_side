8dff51c0127cb3855a0d4ba56039b1c20facdffb
satooru65536 <ryuto4619@gmail.com>
2024-10-12 11:22:11 +0900

    feat

--- src/const/items.ts
+++ src/const/items.ts
@@ -4,33 +4,58 @@ export const itemGroup = {
   food: [
     {
       name: '焼き鳥もも(塩こしょう)',
-      price: 150,
+      price: 120,
       image: '/sample.png',
     },
     {
       name: '焼き鳥ねぎま(塩こしょう)',
-      price: 150,
+      price: 120,
       image: '/sample.png',
     },
     {
       name: '焼き鳥もも(タレ)',
-      price: 150,
+      price: 120,
       image: '/sample.png',
     },
     {
       name: '焼き鳥ねぎま(タレ)',
-      price: 150,
+      price: 120,
+      image: '/sample.png',
+    },
+    {
+      name: '焼き鳥4本セット(各種1本)',
+      price: 400,
+      image: '/sample.png',
+    },
+    {
+      name: '焼き鳥24本セット(各種6本)',
+      price: 2000,
       image: '/sample.png',
     },
   ],
   drink: [
     {
-      name: 'コーラ',
+      name: 'コカコーラ',
+      price: 100,
+      image: '/sample.png',
+    },
+    {
+      name: 'ドクタペッパー',
       price: 100,
       image: '/sample.png',
     },
     {
       name: '칠성사이다(サイダー)',
+      price: 100,
+      image: '/sample.png',
+    },
+    {
+      name: 'ファンタグレープ',
+      price: 100,
+      image: '/sample.png',
+    },
+    {
+      name: '緑茶',
       price: 100,
       image: '/sample.png',
     },
--- src/utils/supabase/db.ts
+++ src/utils/supabase/db.ts
@@ -4,9 +4,9 @@ import { supabase } from './init';
 // import { ofetch } from 'ofetch';
 // import { joinURL } from 'ufo';
 
-const KEY_URL = import.meta.env.VITE_KEY_URL;
+// const KEY_URL = import.meta.env.VITE_KEY_URL;
 
-export async function addOrder(order: OrderItem[], key: string) {
+export async function addOrder(order: OrderItem[], _key: string) {
   // console.log('key:', key);
   // const res = await ofetch(joinURL(KEY_URL, key), {
   //   parseResponse: JSON.parse,
